6184a1a56e3ef7afe2f37098e377be52873a0e3d
kkcjjang <40987311+kkcjjang@users.noreply.github.com>
2018-07-10 18:41:22 +0900

    Main
    First commit

---
+++ 메인창/repos/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace WpfApp1
+{
+    NameAdds nameadds;
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            nameadds = new NameAdds();
+            nameadds = (NameAdds)FindResource("names");
+        }
+        private void insertbutton_Click(object sender, RoutedEventArgs e)
+        {
+            nameadds.Add(new NameAdd(txtName.Text, txtAddress.Text));
+        }
+    }
+    public class NameAdds : ObservableCollection<NameAdd> { }
+    public class NameAdd : INotifyPropertyChanged
+
+    {
+
+        // INotifyPropertyChanged 인터페이스 멤버
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void Notify(string strName)
+
+        {
+
+            if (PropertyChanged != null)
+
+            {
+
+                PropertyChanged(this, new PropertyChangedEventArgs(strName));
+
+            }
+
+        }
+
+
+
+        string name = string.Empty;
+
+        public string Name
+
+        {
+
+            get { return name; }
+
+            set
+
+            {
+
+                name = value;
+
+                Notify("Name");
+
+            }
+
+        }
+
+
+
+        string add = string.Empty;
+
+        public string Add
+
+        {
+
+            get { return add; }
+
+            set
+
+            {
+
+                add = value;
+
+                Notify("Add");
+
+            }
+
+        }
+
+
+
+        public NameAdd() : this("이름입력", "주소입력") { }
+
+        public NameAdd(string name, string add)
+
+        {
+
+            this.name = name;
+
+            this.add = add;
+
+        }
+
+    }
+}
